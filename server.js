d3da77772cca4a99650a9b00144d2f91291578d7
Robert Yuen <dungeonry@hotmail.com>
2017-02-15 15:23:51 -0800

    Landing page changes.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var routes = require("./routes.js");
 
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public/emaillander.ico")));
+app.use(favicon(path.join(__dirname, "public/ry.png")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set("views", path.join(__dirname, "public/views"));
